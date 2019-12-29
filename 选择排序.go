6c93c6470a742dd72180b88adf4cda3e5c2a6900
Chao Ma <machaojms@gmail.com>
2019-12-29 16:46:23 +0800

    Create 选择排序.go

---
+++ 选择排序.go
@@ -0,0 +1,14 @@
+//选择排序（排序10000个随机整数，用时约45ms）
+func selectSort(nums []int) {
+	length := len(nums)
+	for i := 0; i < length; i++ {
+		maxIndex := 0
+		//寻找最大的一个数，保存索引值
+		for j := 1; j < length-i; j++ {
+			if nums[j] > nums[maxIndex] {
+				maxIndex = j
+			}
+		}
+		nums[length-i-1], nums[maxIndex] = nums[maxIndex], nums[length-i-1]
+	}
+}
