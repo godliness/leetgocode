f96da705f880523fdfe788fbaeb1489b68e48420
Chao Ma <machaojms@gmail.com>
2019-12-29 16:45:36 +0800

    Create 冒泡排序.go

---
+++ 冒泡排序.go
@@ -0,0 +1,13 @@
+package main
+
+//冒泡排序（排序10000个随机整数，用时约145ms）
+func bubbleSort(nums []int) {
+	for i := 0; i < len(nums); i++ {
+		for j := 1; j < len(nums)-i; j++ {
+			if nums[j] < nums[j-1] {
+				//交换
+				nums[j], nums[j-1] = nums[j-1], nums[j]
+			}
+		}
+	}
+}
