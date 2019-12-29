a3b4c63f0a08b1e470616b57cb295d89d20d19d9
Chao Ma <machaojms@gmail.com>
2019-12-29 11:33:01 +0800

    Update 交换相邻节点.go

--- 交换相邻节点.go
+++ 交换相邻节点.go
@@ -12,3 +12,21 @@ func swapPairs(head *ListNode) (r *ListNode) {
 	r, head.Next, head.Next.Next = head.Next, swapPairs(head.Next.Next), head
 	return r
 }
+
+func swapPairs2(head *ListNode) *ListNode {
+    newH := &ListNode{}
+    newH.Next = head
+    cur := head
+    prev := newH
+    
+    for cur != nil && cur.Next != nil {
+        prev.Next = cur.Next
+        temp := cur.Next.Next
+        cur.Next.Next = cur
+        cur.Next = temp
+        prev = cur
+        cur = temp
+    }
+    
+    return newH.Next
+}
