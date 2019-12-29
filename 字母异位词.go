2d0298f45cbf9d6abf7307c91db7ee7f387573ae
Chao Ma <machaojms@gmail.com>
2019-12-29 22:33:57 +0800

    Update 字母异位词.go

--- 字母异位词.go
+++ 字母异位词.go
@@ -1,22 +1,15 @@
-package main
 
 func isAnagram(s string, t string) bool {
-
-	tmp := make(map[byte]int)
-
+	m1 := map[byte]int{}
+	m2 := map[byte]int{}
 	if len(s) != len(t) {
 		return false
 	}
+	
 	for i := 0; i < len(s); i++ {
-		tmp[s[i]]++
-		tmp[t[i]]--
+		m1[s[i]] = m1[s[i]] + 1
+		m2[t[i]] = m2[t[i]] + 1
 	}
-	for _, v := range tmp {
-		if v != 0 {
-			return false
-		}
-	}
-
-	return true
-
+	
+	return reflect.DeepEqual(m1, m2)
 }
