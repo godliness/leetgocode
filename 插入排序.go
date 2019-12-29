7925b366620e4ae973f7ad4dc562b26e0ba7b2f7
Chao Ma <machaojms@gmail.com>
2019-12-29 16:48:03 +0800

    Create 插入排序.go

---
+++ 插入排序.go
@@ -0,0 +1,14 @@
+//插入排序（排序10000个整数，用时约30ms）
+func insertSort(nums []int) {
+	for i := 1; i < len(nums); i++ {
+		if nums[i] < nums[i-1] {
+			j := i - 1
+			temp := nums[i]
+			for j >= 0 && nums[j] > temp {
+				nums[j+1] = nums[j]
+				j--
+			}
+			nums[j+1] = temp
+		}
+	}
+}
