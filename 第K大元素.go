78bcb088e5a98e41b1908b68106bd4d00657b4d8
Chao Ma <machaojms@gmail.com>
2019-12-29 16:53:56 +0800

    Update 第K大元素.go

--- 第K大元素.go
+++ 第K大元素.go
@@ -1,55 +1,66 @@
-package main
+package problem0703
 
-import "container/heap"
+import (
+	"container/heap"
+)
 
-// copied from golang doc
-// mininum setup of integer min heap
-type IntHeap []int
+// KthLargest object will be instantiated and called as such:
+// obj := Constructor(k, nums);
+// param_1 := obj.Add(val);
+type KthLargest struct {
+	k    int
+	heap intHeap
+}
 
-func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
-func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+// Constructor 创建 KthLargest
+func Constructor(k int, nums []int) KthLargest {
+	h := intHeap(nums)
+	heap.Init(&h)
 
-func (h *IntHeap) Push(x interface{}) {
-	// Push and Pop use pointer receivers because they modify the slice's length,
-	// not just its contents.
+	for len(h) > k {
+		heap.Pop(&h)
+	}
+
+	return KthLargest{
+		k:    k,
+		heap: h,
+	}
+}
+
+// Add 负责添加元素
+func (kl *KthLargest) Add(val int) int {
+	heap.Push(&kl.heap, val)
+
+	if len(kl.heap) > kl.k {
+		heap.Pop(&kl.heap)
+	}
+
+	return kl.heap[0]
+}
+
+type intHeap []int
+
+func (h intHeap) Len() int {
+	return len(h)
+}
+
+func (h intHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+
+func (h intHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+func (h *intHeap) Push(x interface{}) {
+	// Push 使用 *h，是因为
+	// Push 增加了 h 的长度
 	*h = append(*h, x.(int))
 }
 
-func (h *IntHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
-}
-
-// real thing starts here
-type KthLargest struct {
-	Nums *IntHeap
-	K    int
-}
-
-func Constructor(k int, nums []int) KthLargest {
-	h := &IntHeap{}
-	heap.Init(h)
-	// push all elements to the heap
-	for i := 0; i < len(nums); i++ {
-		heap.Push(h, nums[i])
-	}
-	// remove the smaller elements from the heap such that only the k largest elements are in the heap
-	for len(*h) > k {
-		heap.Pop(h)
-	}
-	return KthLargest{h, k}
-}
-
-func (this *KthLargest) Add(val int) int {
-	if len(*this.Nums) < this.K {
-		heap.Push(this.Nums, val)
-	} else if val > (*this.Nums)[0] {
-		heap.Push(this.Nums, val)
-		heap.Pop(this.Nums)
-	}
-	return (*this.Nums)[0]
+func (h *intHeap) Pop() interface{} {
+	// Pop 使用 *h ，是因为
+	// Pop 减短了 h 的长度
+	res := (*h)[len(*h)-1]
+	*h = (*h)[:len(*h)-1]
+	return res
 }
