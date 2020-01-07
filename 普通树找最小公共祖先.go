fcba0b4cb275c99fe3fbfd9b4f4be843ac2b668c
Chao Ma <machaojms@gmail.com>
2020-01-07 23:14:48 +0800

    Create 普通树找最小公共祖先.go

---
+++ 普通树找最小公共祖先.go
@@ -0,0 +1,22 @@
+ func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+     if root == nil || root == p || root == q {
+         return root
+     }
+     
+     left := lowestCommonAncestor(root.Left, p, q)
+     right := lowestCommonAncestor(root.Right, p, q)
+     
+     if left == nil {
+         return right
+     }
+     
+     if right == nil {
+         return left
+     }
+     
+     if left != nil && right != nil {
+         return root
+     }
+     
+     return nil
+}
