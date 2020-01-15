bc4db551431087d88333f6f61cb5c20ad9ae1078
Chao Ma <machaojms@gmail.com>
2020-01-15 23:52:22 +0800

    Create 生成括号有效组合.go

---
+++ 生成括号有效组合.go
@@ -0,0 +1,19 @@
+func generateParenthesis(n int) []string {
+    if n == 0 { return []string{} }
+    res := []string{}
+    helper(n, n, "", &res)
+    return res
+}
+
+func helper(l, r int, tmp string, res *[]string) {
+    if r < l { return }
+    if l == 0 && r == 0 {
+        *res = append(*res, tmp)
+    }
+    if l > 0 {
+        helper(l-1, r, tmp + "(", res)
+    }
+    if r > 0 {
+        helper(l, r-1, tmp + ")", res)
+    }
+}
