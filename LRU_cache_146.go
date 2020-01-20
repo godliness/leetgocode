5d35e409db4088a3141543f0e209d106b536d983
Chao Ma <machaojms@gmail.com>
2020-01-20 11:12:29 +0800

    Update LRU_cache_146.go

--- LRU_cache_146.go
+++ LRU_cache_146.go
@@ -20,28 +20,28 @@ func Constructor(capacity int) LRUCache {
 
 func (this *LRUCache) Get(key int) int {
     le, ok := this.m[key]
-	if !ok {
-		return -1
-	}
-	this.list.MoveToFront(le)
-	return le.Value.(*KeyVal).val
+    if !ok {
+        return -1
+    }
+    this.list.MoveToFront(le)
+    return le.Value.(*KeyVal).val
 }
 
 
 func (this *LRUCache) Put(key int, value int)  {
     _, ok := this.m[key]
-	if ok {
-		le := this.m[key]
-		this.list.MoveToFront(le)
-		le.Value = &KeyVal{key, value}
-	} else {
-		if this.list.Len() == this.capacity {
-			le := this.list.Back()
-			mkey := le.Value.(*KeyVal).key
-			delete(this.m, mkey)
-			this.list.Remove(le)
-		}
-		this.list.PushFront(&KeyVal{key, value})
-		this.m[key] = this.list.Front()
+    if ok {
+	le := this.m[key]
+	this.list.MoveToFront(le)
+	le.Value = &KeyVal{key, value}
+    } else {
+	if this.list.Len() == this.capacity {
+	    le := this.list.Back()
+	    mkey := le.Value.(*KeyVal).key
+	    delete(this.m, mkey)
+	    this.list.Remove(le)
 	}
+	this.list.PushFront(&KeyVal{key, value})
+	this.m[key] = this.list.Front()
+    }
 }
