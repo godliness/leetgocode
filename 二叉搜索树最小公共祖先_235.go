0335e02adc87c01190dc863ee59c8eff196e597d
Chao Ma <machaojms@gmail.com>
2020-01-22 20:46:05 +0800

    Update 二叉搜索树最小公共祖先_235.go

--- 二叉搜索树最小公共祖先_235.go
+++ 二叉搜索树最小公共祖先_235.go
@@ -1,1 +1,13 @@
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	return helper(root, p.Val, q.Val)
+}
 
+func helper(root *TreeNode, p, q int) *TreeNode {
+	r := root.Val
+	if r > p && r > q {
+		return helper(root.Left, p, q)
+	} else if r < p && r < q {
+		return helper(root.Right, p, q)
+	}
+	return root
+}
