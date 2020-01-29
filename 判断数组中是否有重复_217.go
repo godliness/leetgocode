642a90a405bbc3e34181a68a65bda6613eb89785
Chao Ma <machaojms@gmail.com>
2020-01-29 10:08:47 +0800

    Update 判断数组中是否有重复_217.go

--- 判断数组中是否有重复_217.go
+++ 判断数组中是否有重复_217.go
@@ -2,8 +2,7 @@ func containsDuplicate(nums []int) bool {
     var mp = make(map[int]int)
     
     for idx, value := range nums {
-        _, ok := mp[value]
-        if ok {
+        if _, ok := mp[value]; ok {
             return true
         }
         mp[value] = idx
