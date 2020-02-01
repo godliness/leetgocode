53579b89021af963b433cfdfd66c6381eb719f25
Chao Ma <machaojms@gmail.com>
2020-02-01 12:40:32 +0800

    Update 字符串的最大公因子_1071.go

--- 字符串的最大公因子_1071.go
+++ 字符串的最大公因子_1071.go
@@ -1,13 +1,20 @@
 func gcdOfStrings(str1 string, str2 string) string {
-    prefix := str1
-    
-    for prefix != "" {
-        if strings.HasPrefix(str1, prefix) && strings.HasPrefix(str2, prefix) &&
-        len(str1) % len(prefix) == 0 && len(str2) % len(prefix) == 0 {
-            return prefix
+    if len(str1) == 0 || str1 == str2 {
+        return str1
+    }
+    if len(str2) == 0 {
+        return str2
+    }
+    if len(str1) > len(str2) {
+        x := strings.Index(str1, str2)
+        if x > -1 {
+            return gcdOfStrings(str2, str1[x+len(str2):])
         }
-        
-        prefix = prefix[:len(prefix)-1]
+    } else {
+        y := strings.Index(str2, str1)
+        if y > -1 {
+            return gcdOfStrings(str1, str2[y+len(str1):])
+        }
     }
     
     return ""
