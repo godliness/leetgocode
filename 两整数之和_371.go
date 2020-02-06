322299a93a1a8c25886292e25162075ce6694e65
Chao Ma <machaojms@gmail.com>
2020-02-06 10:05:27 +0800

    Update 两整数之和_371.go

--- 两整数之和_371.go
+++ 两整数之和_371.go
@@ -2,5 +2,10 @@ func getSum(a int, b int) int {
     if b == 0{
         return a
     } 
-    return getSum(a^b, (a & b)<<1)
+    return getSum(a ^ b, (a & b)<<1)
 }
+
+a + b 的问题拆分为 (a 和 b 的无进位结果) + (a 和 b 的进位结果)
+无进位加法使用异或运算计算得出
+进位结果使用与运算和移位运算计算得出
+循环此过程，直到进位为 0
