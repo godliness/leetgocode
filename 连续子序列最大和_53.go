e5ac1f3454d7aa6db3a807d4ef8148e65c01dc72
Chao Ma <machaojms@gmail.com>
2020-02-09 22:34:41 +0800

    Update and rename 最大连续子序列和_53.go to 连续子序列最大和_53.go

--- 连续子序列最大和_53.go
+++ 连续子序列最大和_53.go
@@ -3,8 +3,8 @@ func maxSubArray(nums []int) int {
     dp[0] = nums[0]
     ans := dp[0]
     for i := 1; i < len(nums); i++ {
-        dp[i] = max(dp[i-1]+nums[i], nums[i])
-        ans = max(ans, dp[i])
+        dp[i] = max(dp[i-1]+nums[i], nums[i]) // dp[i] 从不确定的位置到第i的位置子数组的最大和
+        ans = max(ans, dp[i]) // 选出dp[i]中最大的
     }
     return ans    
 }
