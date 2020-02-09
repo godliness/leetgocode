a757c0e6a3852d492781dd6585f3b396ed0bf17f
Chao Ma <machaojms@gmail.com>
2020-02-09 22:26:30 +0800

    Update 兑换硬币_322.go

--- 兑换硬币_322.go
+++ 兑换硬币_322.go
@@ -1,25 +1,18 @@
 func coinChange(coins []int, amount int) int {
 	dp := make([]int, amount+1)
-  dp[0] = 0
+	dp[0] = 0
 	for i := 1; i <= amount; i++ {
 		dp[i] = amount + 1
 		for _, coin := range coins {
-            if coin <= i {
-                dp[i] = min(dp[i], dp[i-coin] + 1)
-            }
-        }
+			if coin <= i {
+				dp[i] = min(dp[i], dp[i-coin]+1)
+			}
+		}
 	}
 
-  if dp[amount] > amount {
-     return -1
-  }
-    
-	return dp[amount]
-}
+	if dp[amount] > amount {
+		return -1
+	}
 
-func min(a, b int) int {
-    if a < b {
-        return a
-    }
-    return b
+	return dp[amount]
 }
