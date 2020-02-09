5ba1f4752d9f4d123a6c3958dbe22a8460b494ec
Chao Ma <machaojms@gmail.com>
2020-02-09 18:30:56 +0800

    Create 最长上升子序列_300.go

---
+++ 最长上升子序列_300.go
@@ -0,0 +1,29 @@
+func lengthOfLIS(nums []int) int {
+	if nums == nil || len(nums) == 0 {
+		return 0
+	}
+
+	n, max := len(nums), 1
+	d := make([]int, n)
+	d[0] = 1
+
+	for i := 1; i < n; i++ {
+		for j := 0; j < i; j++ {
+			cur := 1
+			if nums[i] > nums[j] {
+				cur = d[j] + 1
+			}
+			d[i] = Max(d[i], cur)
+		}
+		max = Max(max, d[i])
+	}
+
+	return max
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
