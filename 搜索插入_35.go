26c8d2146ca799569708ccc0f93f76975d46c883
Chao Ma <machaojms@gmail.com>
2020-02-10 12:22:01 +0800

    Create 搜索插入_35.go

---
+++ 搜索插入_35.go
@@ -0,0 +1,16 @@
+func searchInsert(nums []int, target int) int {
+  l, r := 0, len(nums)-1
+  
+  for l <= r {
+    m := l + (r-l)/2
+    if (nums[m] < target) {
+      l = m+1
+    } else if (nums[m] > target) {
+      r = m-1
+    } else {
+      return m
+    }
+  }
+  
+  return l
+}
