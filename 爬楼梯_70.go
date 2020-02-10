6972bc46f7b9345290e7f70289f8c443ce36381a
Chao Ma <machaojms@gmail.com>
2020-02-10 14:53:41 +0800

    Update 爬楼梯_70.go

--- 爬楼梯_70.go
+++ 爬楼梯_70.go
@@ -1,33 +1,12 @@
 func climbStairs(n int) int {
-    cache := make(map[int]int)
-    cache[1] = 1
-    cache[2] = 2
-    
-    return csrec(n, cache)
-}
-
-func csrec(n int, cache map[int]int) int {
-    if val, ok := cache[n]; ok {
-        return val
-    }
-    
-    ret := csrec(n-1, cache) + csrec(n-2, cache)
-    cache[n] = ret
-    return ret
-}
-
-===========
-
-
-func climbStairs(n int) int {
      if n<=1 {
         return 1
     }
     dp := make([]int, n)
     dp[0]=1
     dp[1]=2
-    for i:=2;i<n ;i++  {
-        dp[i]=dp[i-1]+dp[i-2]
+    for i:=2;i<n ;i++  {       
+        dp[i]=dp[i-1]+dp[i-2] // dp[i]代表走到第i台阶时候所用的走法数  公式：我走到第i个台阶的总走法等于走到第i-1阶时与第i-2阶时用过的方法总和。
     }
     return dp[n-1]
 }
