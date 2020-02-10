dcd4bcb90a848efb72f01688d9f8f042949f69bd
Chao Ma <machaojms@gmail.com>
2020-02-10 16:58:30 +0800

    Update 连续子序列最大和_53.go

--- 连续子序列最大和_53.go
+++ 连续子序列最大和_53.go
@@ -3,7 +3,7 @@ func maxSubArray(nums []int) int {
     dp[0] = nums[0]
     ans := dp[0]
     for i := 1; i < len(nums); i++ {
-        dp[i] = max(dp[i-1]+nums[i], nums[i]) // dp[i] 从不确定的位置到第i的位置子数组的最大和
+        dp[i] = max(dp[i-1]+nums[i], nums[i]) // dp[i] 从不确定的位置到第i的位置子数组的最大和， 如果加上了前面数列的和还没我大，那就不加，继续保持我自己
         ans = max(ans, dp[i]) // 选出dp[i]中最大的
     }
     return ans    
