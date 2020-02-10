15c14e9c2fb198ff1feb78004df212eae215dfbc
Chao Ma <machaojms@gmail.com>
2020-02-10 20:23:23 +0800

    Update 最长上升子序列_300.go

--- 最长上升子序列_300.go
+++ 最长上升子序列_300.go
@@ -11,7 +11,7 @@ func lengthOfLIS(nums []int) int {
 		for j := 0; j < i; j++ {
 			cur := 1
 			if nums[i] > nums[j] { // nums[i]前面的 0...i-1的元素是否比nums[i]小，若小的话，计数+1
-				cur = d[j] + 1 // d[j] 代表到第j个位置的数已经有多少个上升数字了， 目前nums[i] > nums[j]说明又多了一个，所以是
+				cur = d[j] + 1 // d[j] 代表到第j个位置的数已经有多少个上升数字了(算上j)， 目前nums[i] > nums[j]说明又多了一个，所以是
 			}
 			d[i] = Max(d[i], cur) // 我们定位在d[i]的位置，然后其次查找前面0-j的上升子序列和+1的结果，选一个最大的，因为可能d[j] 0-j 有大有小
 		}
