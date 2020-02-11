c2292c63d479fc403b5a63751f93a31dd8b304d6
Chao Ma <machaojms@gmail.com>
2020-02-11 20:20:11 +0800

    Update 二叉树最大深度_104.go

--- 二叉树最大深度_104.go
+++ 二叉树最大深度_104.go
@@ -3,7 +3,7 @@ func maxDepth(root *TreeNode) int {
         return 0
     }
     
-    return 1 + max(maxDepth(root.Right), maxDepth(root.Left))
+    return 1 + max(maxDepth(root.Right), maxDepth(root.Left)) // 一直递归到最末尾，一次上翻+1，得到最大深度
 }
 
 func max(x, y int) int {
