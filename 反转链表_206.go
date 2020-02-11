55e7985922758e8d84ae37a8d9a5b065eb3cb7b4
Chao Ma <machaojms@gmail.com>
2020-02-11 11:53:07 +0800

    Update 反转链表_206.go

--- 反转链表_206.go
+++ 反转链表_206.go
@@ -16,7 +16,7 @@ func reverseList(head *ListNode) *ListNode {
 func reverseList(head *ListNode) *ListNode {
     var prev *ListNode
     for head != nil {
-        nextTemp := head.Next
+        nextTemp := head.Next // 先预留head.Next以后用
         head.Next = prev // head.Next 指针要指向前面的节点，而且prev变量的意义是接收head的，而head要被赋值成head.Next，以供下一次循环使用。
         prev = head
         head = nextTemp // 典型的 head = head.Next 遍历链表
