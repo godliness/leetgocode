0982694659080b03259074a92683b7a75450a51a
Chao Ma <machaojms@gmail.com>
2020-02-11 20:03:12 +0800

    Update 首缺失正整数_41.go

--- 首缺失正整数_41.go
+++ 首缺失正整数_41.go
@@ -1,14 +1,14 @@
 func firstMissingPositive(nums []int) int {
     hashMap := make(map[int]struct{})
     for i:=0; i < len(nums); i++ {
-        hashMap[nums[i]] = struct{}{}
+        hashMap[nums[i]] = struct{}{}  
     }
     
-    for i:=1; i<=len(nums); i++ {
-        if _, ok := hashMap[i]; !ok {
+    for i:=1; i<=len(nums); i++ { // i=1因为是最小正整数
+        if _, ok := hashMap[i]; !ok { // 从1找起，按顺序找，在不在map中，若不在map中就直接返回该数。
             return i
         }
     } 
     
-    return len(nums) + 1
+    return len(nums) + 1 
 }
