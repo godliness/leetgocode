b47a028dca393f0011755d72fde9af26f6622561
Chao Ma <machaojms@gmail.com>
2020-02-11 20:22:39 +0800

    Update 二叉树最小深度_111.go

--- 二叉树最小深度_111.go
+++ 二叉树最小深度_111.go
@@ -2,7 +2,7 @@ func minDepth(root *TreeNode) int {
     if root == nil {
 	return 0
     }
-    left, right := minDepth(root.Left), minDepth(root.Right)
+    left, right := minDepth(root.Left), minDepth(root.Right) 
     if left == 0 || right == 0 {
 	return 1 + left + right
     }
