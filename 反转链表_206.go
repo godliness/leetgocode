d2768658cc3bb4919d39c097a408ab71ab168a57
Chao Ma <machaojms@gmail.com>
2020-02-11 11:52:09 +0800

    Update 反转链表_206.go

--- 反转链表_206.go
+++ 反转链表_206.go
@@ -17,9 +17,9 @@ func reverseList(head *ListNode) *ListNode {
     var prev *ListNode
     for head != nil {
         nextTemp := head.Next
-        head.Next = prev
+        head.Next = prev // head.Next 指针要指向前面的节点，而且prev变量的意义是接收head的，而head要被赋值成head.Next，以供下一次循环使用。
         prev = head
-        head = nextTemp
+        head = nextTemp // 典型额 head = head.Next 遍历链表
     }
     return prev
 }
