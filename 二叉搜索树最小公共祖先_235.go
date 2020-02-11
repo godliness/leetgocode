cd5a5179cab4cf7a6f8e0a1b3ca3343eb3fc7245
Chao Ma <machaojms@gmail.com>
2020-02-11 16:57:25 +0800

    Update 二叉搜索树最小公共祖先_235.go

--- 二叉搜索树最小公共祖先_235.go
+++ 二叉搜索树最小公共祖先_235.go
@@ -2,12 +2,13 @@ func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
 	return helper(root, p.Val, q.Val)
 }
 
+// 二叉树特点，右子树永远大于根节点，左子树永远小于根节点
 func helper(root *TreeNode, p, q int) *TreeNode {
-	r := root.Val
-	if r > p && r > q {
-		return helper(root.Left, p, q)
-	} else if r < p && r < q {
+	r := root.Val //从上往下找
+	if r > p && r > q { // 根节点如果同时大于p, q 那么说明公共祖先应该在左子树，说明找反了
+		return helper(root.Left, p, q) 
+	} else if r < p && r < q { // 根节点如果同时小于p, q 那么说明公共祖先应该在右子树，说明找反了
 		return helper(root.Right, p, q)
 	}
-	return root
+	return root // root正好 > p && < q || <p && > q 说明找到了 p q 正好在root的两边
 }
