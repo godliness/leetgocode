beb71d38b7fdb29086437bd117770bf735f8264c
Chao Ma <machaojms@gmail.com>
2020-02-11 20:10:12 +0800

    Update 首缺失正整数_41.go

--- 首缺失正整数_41.go
+++ 首缺失正整数_41.go
@@ -5,10 +5,11 @@ func firstMissingPositive(nums []int) int {
     }
     
     for i:=1; i<=len(nums); i++ { // i=1因为是最小正整数
-        if _, ok := hashMap[i]; !ok { // 从1找起，按顺序找，在不在map中，若不在map中就直接返回该数。
+        if _, ok := hashMap[i]; !ok { // 从1找起，按顺序找，在不在map中，若不在map中就直接返回该数。因为数字是能从1开始连续往后
+                                      // 1,2,3,4,5这样占位 若不在该map中那就是它了
             return i
         }
     } 
     
-    return len(nums) + 1 
+    return len(nums) + 1 // 若1,2,3,4,5都给占用了，那只能返回6，因为他是目前缺失唯一最小的整数
 }
