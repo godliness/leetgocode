3c972aa49b1786c61877b09d22ba3087bd044eea
Chao Ma <machaojms@gmail.com>
2020-03-21 14:46:42 +0800

    Create 相同树_100.go

---
+++ 相同树_100.go
@@ -0,0 +1,13 @@
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	//测试空节点
+	if p == nil && q == nil {
+		return true
+	} else if p == nil || q == nil {
+		return false
+	}
+
+	if p.Val != q.Val {
+		return false
+	}
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
